d5fd9aef1d639fe2d1d888d9b371c559c9e66af4
tejava317 <frogjj@kaist.ac.kr>
2025-06-03 20:36:00 +0900

    refact: Merge RepositoryAnalyzer and TestcaseGenerator

--- src/server.py
+++ src/server.py
@@ -1,5 +1,18 @@
 from mcp.server.fastmcp import FastMCP
 from tools.github import RepositoryAnalyzer
+from openai import OpenAI
+from typing import Any
+from dotenv import load_dotenv
+import subprocess
+import os
+import httpx
+import ast
+import esprima
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
+
+PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 
 mcp = FastMCP(
     name="GitHub Repository Agent",
@@ -9,11 +22,98 @@ mcp = FastMCP(
 )
 
 repository_analyzer =  RepositoryAnalyzer()
+client = OpenAI(api_key=PERPLEXITY_API_KEY, base_url="https://api.perplexity.ai")
 
 @mcp.tool()
 def load_repository(repo_owner: str, repo_name: str) -> str:
     """Load information about a GitHub repository"""
     return repository_analyzer.load_documents(repo_owner, repo_name)
+
+@mcp.tool()
+def generate_test(code:str) -> str:
+    """Generate a test of the given javascript code.
+    Args:
+        code: js script
+    """
+    # Here you would implement logic to generate a test based on the analysis
+    # For simplicity, we will just return the analysis as the test code
+    # code = None
+    # try:
+    #     with open(codepath, 'r', encoding='utf-8') as f:
+    #         code = f.read()
+    # except Exception as e:
+    #     return str(e)
+    analysis = esprima.parseScript(code)
+    # analysis = code
+    
+    # process = subprocess.Popen(
+    # "ollama run gemma3:4b",  
+    # stdin=subprocess.PIPE,
+    # stdout=subprocess.PIPE,
+    # stderr=subprocess.PIPE,
+    # text=True,  # Python 3.7 이상에서 텍스트 모드 사용
+    # bufsize=10,
+    # encoding='utf-8' # 라인 단위 버퍼링
+    # )
+
+    user_input = f"""code: {code}'\n---------------'\n' analysis:{analysis},
+give me EXACT, PROPER, and VARIOUS test cases."""
+    # outs, errs = process.communicate(input=user_input, timeout=60)
+    
+    messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are the best expert in code analysis and generation. "
+            "You MUST generate a test based on the analysis provided."
+            "The test should be a as VARIOUS as possible and EXACT."
+            "You must use the following format."
+            "- test case name: <name of test case>"
+            "- test code: <code of the test>"
+        ),  
+    },
+    {   
+        "role": "user",
+        "content": (
+            "Please crate a good test code based on the analysis provided."
+            f"analysis: {analysis}"
+            f"code: {code}"
+        ),
+    },
+    ]
+
+    # chat completion without streaming
+    response = client.chat.completions.create(
+        model="sonar-pro",
+        messages=messages,
+    )
+    messages2 = [
+    {
+        "role": "system",
+        "content": (
+            "You are the best expert in code analysis and generation. "
+            "You MUST generate a test based on the analysis provided."
+            "The test should be a as VARIOUS as possible and EXACT."
+            "You must use the following format."
+            "- test case name: <name of test case>"
+            "- test code: <code of the test>"
+        ),  
+    },
+    {   
+        "role": "user",
+        "content": (
+            f"analyze your previous response {str(response)} and modify properly, add new test cases."
+            f"analysis: {analysis}"
+            f"code: {code}"
+        ),
+    },
+    ]
+    # modify again.
+    response2 = client.chat.completions.create(
+        model="sonar-pro",
+        messages=messages2,
+    )
+    return str(response)
 
 if __name__ == "__main__":
     # Use 'mcp dev src/server.py' to start MCP Inspector
--- src/tools/github.py
+++ src/tools/github.py
@@ -1,22 +1,26 @@
 from langchain_community.document_loaders import GitLoader
 import tempfile
+import shutil
 
 class RepositoryAnalyzer:
     def __init__(self):
         self.repo_owner = ""
         self.repo_name = ""
-        self.repo_path = tempfile.mkdtemp()
+        self.repo_path = None
         self.loader = None
         self.documents = None
-        self.valid_file_extensions = [".py"]  # TODO: Add more file extensions
+        self.valid_file_extensions = [".js", ".ts", ".jsx", ".tsx"]  # TODO: Add more file extensions
 
     def load_documents(self, repo_owner, repo_name):
         self.repo_owner = repo_owner
         self.repo_name = repo_name
+        self.repo_path = tempfile.mkdtemp()
         self.loader = GitLoader(
             clone_url=f"https://github.com/{self.repo_owner}/{self.repo_name}",
             repo_path=self.repo_path,
-            file_filter=lambda x: x.endswith(tuple(self.valid_file_extensions))
+            # file_filter=lambda x: x.endswith(tuple(self.valid_file_extensions)),
+            branch="master"
         )
         self.documents = self.loader.load()
+        shutil.rmtree(self.repo_path)
         return self.documents
