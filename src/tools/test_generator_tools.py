54b885ee0a8bc15a9e2d3c13640ceccc8e71a733
hbg1345 <hbg1345@gmail.com>
2025-06-13 21:06:22 +0900

    Feat: done

--- src/tools/test_generator_tools.py
+++ src/tools/test_generator_tools.py
@@ -42,12 +42,12 @@ def _test_and_repeat(code_path: str, test_code_path: str, save_code_path: str):
     """execute test code and return test result."""
     """test_code_path must be absolute path."""
     """save_code_path: must be absolute path where syntatically modified test_code is saved"""
-    if not test_code_path.endswith('.test.js'):
-        return "The test_code_path is not valid. It must ends with .test.js"
-    if not save_code_path.endswith('.test.js'):
-        return "The save path must ends with .test.js"
-    if not code_path.endswith('.js'):
-        return "The code path must ends with .js."
+    if not test_code_path.endswith('.test.js') or not not test_code_path.endswith('.test.ts'):
+        return "The test_code_path is not valid. It must ends with .test.js or .test.ts"
+    if not save_code_path.endswith('.test.js') or not save_code_path.endswith('.test.ts'):
+        return "The save path must ends with .test.js or .test.ts"
+    if not code_path.endswith('.js') or not code_path.endswith('.ts'):
+        return "The code path must ends with .js or .ts"
     if code_path == test_code_path:
         return "The three paths must be mutually different."
     if test_code_path == save_code_path:
@@ -79,10 +79,10 @@ def _test_and_repeat(code_path: str, test_code_path: str, save_code_path: str):
 
 def _generate_test_from_raw_code(code:str, code_file_path:str, test_code_path, repo_tree:str) -> str:
     analysis = ''
-    if not code_file_path.endswith('.js'):
-        return "The test code must be javascript file."
-    if not test_code_path.endswith('test.js'):
-        return "The test code path must be jest test file, so it must ends with test.js"
+    if not code_file_path.endswith('.js') or not code_file_path.endswith('.ts'):
+        return "The test code must be javascript/typescript file."
+    if not test_code_path.endswith('test.js') or not test_code_path.endswith('test.ts'):
+        return "The test code path must be jest test file, so it must ends with test.js or test.ts"
     try:
         ast = esprima.parseModule(code)
         analysis = json.dumps(ast.toDict(), indent=2, ensure_ascii=False)
